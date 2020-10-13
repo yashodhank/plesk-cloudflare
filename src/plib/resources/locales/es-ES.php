fe6dfe1c007e5f9d19081e897c877e3a6e0cb5db
virmix <javi200482@gmail.com>
2020-10-13 20:23:53 +0200

    Add files via upload

---
+++ src/plib/resources/locales/es-ES.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Copyright 2020 Javier Puerto
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * @copyright 2020
+ * @author    Javier Puerto
+ * @license   MIT
+ */
+
+$messages = [
+    'actionsColumn' => 'Acciones',
+    'buttonNo' => 'No',
+    'buttonYes' => 'Si',
+    'pageTitle' => 'DNS de Cloudflare',
+    'indexPageTitle' => 'Credenciales de Cloudflare',
+    'emailPrivateKeyInvalidError' => 'El correo electrónico y / la clave API no son válidos',
+    'emailLabel' => 'Correo electrónico',
+    'privateKeyLabel' => 'Clave API',
+    'cloudflareCredentials' => 'Credenciales API de Cloudflare',
+    'cloudflareDescriptionWindow' => 'Ingrese sus credenciales de API de Cloudflare para comenzar. Puede generarlos en su panel de control de Cloudflare. ',
+    'authDataSaved' => 'Las credenciales han sido guardadas',
+    'domains' => 'Dominios',
+    'domainNotFound' => 'Este dominio no está en Cloudflare',
+    'domainsList' => 'Lista de dominios',
+    'domainsColumn' => 'Dominio',
+    'enabledColumn' => 'Habilitado',
+    'on' => 'Activado',
+    'off' => 'Desactivado',
+    'enableDomain' => 'Dominio %1 ya se encuentra activado',
+    'enableDomainButton' => 'Activar',
+    'enableDomainsButton' => 'Habilitar sincronización automática',
+    'enableDomainsHint' => 'Habilitar dominios seleccionados',
+    'disableDomain'                => 'Dominio %1 ya se encuentra desactivado',
+    'disableDomainButton' => 'Desactivar',
+    'disableDomainsButton' => 'Deshabilitar sincronización automática',
+    'disableDomainsHint' => 'Deshabilitar dominios seleccionados',
+    'domainEnabled' => 'El dominio seleccionado ha sido habilitado',
+    'domainDisabled' => 'El dominio seleccionado ha sido deshabilitado',
+    'domainsEnabled' => 'Los dominios seleccionados han sido habilitados',
+    'domainsDisabled' => 'Los dominios seleccionados han sido deshabilitados',
+    'syncDomainsButton' => 'Sincronizar dominios manualmente',
+    'syncDomainsHint' => 'Sincronizar manualmente los dominios seleccionados',
+    'autoSync' => 'Sincronización automática',
+    'enableAutoSync' => 'Habilitar sincronización automática',
+    'disableAutoSync' => 'Desactivar sincronización automática',
+    'domainsProcessed' => 'Los dominios han sido procesados',
+    'overrideTtlLabel' => 'Asignar TTL',
+    'overrideTtlHint' => 'Introduzca un valor en segundos. Déjelo en blanco para usar el valor predeterminado de Plesk.',
+    'syncNewDomainsLabel' => 'Sincronizar nuevos dominios automáticamente',
+    'proxyByDefaultLabel' => 'Proxy a través de Cloudflare por defecto',
+];
