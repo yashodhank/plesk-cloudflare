0cccf587311d264c066513a0ead41adf3e8ff5f6
abdullah azad <gokabdullah@gmail.com>
2020-12-08 11:27:59 +0300

    Turkish translation

---
+++ src/plib/resources/locales/tr-TR.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Copyright 2018-2019 Michael Dekker
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * @copyright 2018-2019 Michael Dekker
+ * @author    Michael Dekker <info@trendweb.io>
+ * @license   MIT
+ */
+
+$messages = [
+    'actionsColumn'               => 'Seçenekler',
+    'buttonNo'                    => 'Hayır',
+    'buttonYes'                   => 'Evet',
+    'pageTitle'                   => 'Cloudflare DNS',
+    'indexPageTitle'              => 'Cloudflare Ayarları',
+    'emailPrivateKeyInvalidError' => 'Email ve/veya API key\'iniz geçersiz',
+    'emailLabel'                  => 'Email',
+    'privateKeyLabel'             => 'API Key',
+    'cloudflareCredentials'       => 'Cloudflare API Ayarları',
+    'cloudflareDescriptionWindow' => 'LütfenCloudflare API bilgirinizi yazınız. Cloudflare Control Panel\'den API key\'inizi üretebilirsiniz..',
+    'authDataSaved'               => 'API bilgileri başarıyla kaydedildi',
+    'domains'                     => 'Alanadları',
+    'domainNotFound'              => 'Bu alanadı Cloudflare hesabınızda bulunmamakta',
+    'domainsList'                 => 'Alanadı Listesi',
+    'domainsColumn'               => 'Alanadı',
+    'enabledColumn'               => 'Aktif',
+    'on'                          => 'Aktif',
+    'off'                         => 'Pasif',
+    'enableDomain'                => '%1 aktifleştirildi',
+    'enableDomainButton'          => 'Aktifleştir',
+    'enableDomainsButton'         => 'Otomatik senkronizasyonu aktifleştir',
+    'enableDomainsHint'           => 'Seçili alanadlarını aktif et',
+    'disableDomain'               => '%1 pasifleştirildi',
+    'disableDomainButton'         => 'Pasifleştir',
+    'disableDomainsButton'        => 'Otomatik senkronizasyonu pasifleştir',
+    'disableDomainsHint'          => 'Seçili alanalarını pasifleştir',
+    'domainEnabled'               => 'Seçili alanadı aktifleştirildi',
+    'domainDisabled'              => 'Seçili alanadı pasifleştirildi',
+    'domainsEnabled'              => 'Seçili alanadları aktifleştirildi',
+    'domainsDisabled'             => 'Seçili alanadları pasifleştirildi',
+    'syncDomainsButton'           => 'Alanadlarını manuel senkronize et',
+    'syncDomainsHint'             => 'Seçili alanadlarını manuel senkronize et',
+    'autoSync'                    => 'Otomatik senkronizasyon',
+    'enableAutoSync'              => 'Otomatik senkronizasyon aktif',
+    'disableAutoSync'             => 'Otomatik senkronizasyon pasif',
+    'domainsProcessed'            => 'Alanadı ayarı düzenlendi',
+    'overrideTtlLabel'            => 'TTL değerini değiştir',
+    'overrideTtlHint'             => 'Lütfen saniye cisinden değer yazınız. Plesk\'in varsayılan değerini kullanmak için boş bırakabilirsiniz.',
+    'syncNewDomainsLabel'         => 'Yeni alanadlarını otomatik olarak senkronize et',
+    'proxyByDefaultLabel'         => 'Proxy through Cloudflare özelliği varsayılan olarak aktif olsun',
+];
